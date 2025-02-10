c580f237753f91c205b1623f3a8e7311f3cd57ea
Mohamed amine Tlili <foxweb585@gmail.com>
2025-02-10 18:07:47 +0100

    -

--- src/helper/mail.service.ts
+++ src/helper/mail.service.ts
@@ -24,7 +24,7 @@ export class MailService {
   }
 
   async sendActivationEmail(email: string, activationToken: string) {
-    const activationLink = `http://localhost:3000/auth/activate/${activationToken}`;
+    const activationLink = `http://localhost:4200/validate-account/${activationToken}`;
 
     const mailOptions = {
       from: `${process.env.EMAIL_USER}`,
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,8 +1,8 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from '../entities/user.entity';
-import { logger } from '../config/logger.config';
+import { User } from '../../entities/user.entity';
+import { logger } from '../../config/logger.config';
 import { ClientKafka } from '@nestjs/microservices';
 import { Inject } from '@nestjs/common';
 
